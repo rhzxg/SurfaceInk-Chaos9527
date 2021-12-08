41a874d1f6bf4658cc6237a76ff46eb9de816b32
郑相关 <codingdogzxg@gmail.com>
2021-12-08 16:13:15 +0800

    transfer functionality
    functions are integrated to MainPage.xaml.cpp

--- Solution/SurfaceInk/SurfaceInk/Configuration.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pch.h"
-
-class HelperFunctions
-{
-public:
-    static void UpdateCanvasSize(Windows::UI::Xaml::FrameworkElement^ root, Windows::UI::Xaml::FrameworkElement^ output, Windows::UI::Xaml::FrameworkElement^ inkCanvas);
-};
