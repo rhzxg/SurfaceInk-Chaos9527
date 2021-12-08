f534690c123c48f2d35a81486344b985cb47c7f2
CodingDogzxg <codingdogzxg@gmail.com>
2021-12-08 12:41:20 +0800

    publish preview

--- Solution/SurfaceInk/SurfaceInk/MainPage.xaml.cpp
+++ Solution/SurfaceInk/SurfaceInk/MainPage.xaml.cpp
@@ -6,6 +6,7 @@
 #include "pch.h"
 #include "MainPage.xaml.h"
 #include "Configuration.h"
+#include <WindowsNumerics.h>
 
 using namespace SurfaceInk;
 
@@ -16,15 +17,215 @@ using namespace Windows::UI::Core;
 using namespace Windows::UI::Xaml;
 using namespace Windows::UI::Xaml::Controls;
 
+using namespace Platform::Collections;
+using namespace Windows::Foundation::Numerics;
+using namespace Windows::UI::Input::Inking;
+using namespace Windows::UI::Input::Inking::Core;
+using namespace Windows::UI::Xaml::Navigation;
+
 MainPage::MainPage()
 {
 	InitializeComponent();
 	inkCanvas->InkPresenter->InputDeviceTypes =
 		CoreInputDeviceTypes::Mouse |
 		CoreInputDeviceTypes::Pen;
+	inkCanvas->InkPresenter->InputDeviceTypes = CoreInputDeviceTypes::Mouse | CoreInputDeviceTypes::Pen | CoreInputDeviceTypes::Touch;
+	inkCanvas->InkPresenter->StrokesCollected += ref new TypedEventHandler<InkPresenter^, InkStrokesCollectedEventArgs^>(this, &MainPage::InkPresenter_StrokesCollected);
 }
 
 void MainPage::OnSizeChanged(Object^ sender, SizeChangedEventArgs^ e)
 {
 	HelperFunctions::UpdateCanvasSize(RootGrid, outputGrid, inkCanvas);
 }
+
+//void MainPage::OnNavigatingFrom(NavigatingCancelEventArgs^ e)
+//{
+//	StopReplay();
+//}
+
+void MainPage::InkPresenter_StrokesCollected(InkPresenter^ sender, InkStrokesCollectedEventArgs^ e)
+{
+    /*IVectorView<InkStroke^>^ strokes = e->Strokes;
+    String^ log = L"Strokes collected: " + strokes->Size.ToString();
+
+    auto timestampFormatter = ref new Windows::Globalization::DateTimeFormatting::DateTimeFormatter("day month year hour minute second");
+
+    for (InkStroke^ stroke : strokes)
+    {
+        log += "\nId: " + stroke->Id.ToString() +
+            ", StartedTime: " + timestampFormatter->Format(stroke->StrokeStartedTime->Value) +
+            ", Duration: " + (stroke->StrokeDuration->Value.Duration / 10000L) + "ms";
+    }
+    rootPage->NotifyUser(log, NotifyType::StatusMessage);*/
+}
+
+void MainPage::OnReset(Object^ sender, RoutedEventArgs^ e)
+{
+    StopReplay();
+    inkCanvas->InkPresenter->StrokeContainer->Clear();
+    ClearCanvasStrokeCache();
+    // rootPage->NotifyUser("Cleared Canvas", NotifyType::StatusMessage);
+}
+
+void MainPage::ClearCanvasStrokeCache()
+{
+    // Workaround for builds prior to 17650.
+    // Throw away the old inkCanvas and create a new one to avoid accumulation of old strokes.
+    outputGrid->Children->Clear();
+    inkCanvas = ref new Windows::UI::Xaml::Controls::InkCanvas();
+    outputGrid->Children->Append(inkCanvas);
+    inkCanvas->InkPresenter->InputDeviceTypes = CoreInputDeviceTypes::Mouse | CoreInputDeviceTypes::Pen | CoreInputDeviceTypes::Touch;
+    inkCanvas->InkPresenter->StrokesCollected += ref new TypedEventHandler<InkPresenter^, InkStrokesCollectedEventArgs^>(this, &MainPage::InkPresenter_StrokesCollected);
+    HelperFunctions::UpdateCanvasSize(RootGrid, outputGrid, inkCanvas);
+}
+
+DateTime GetCurrentDateTime()
+{
+    FILETIME fileTime;
+    GetSystemTimeAsFileTime(&fileTime);
+    return DateTime{ (static_cast<uint64_t>(fileTime.dwHighDateTime) << 32) | fileTime.dwLowDateTime };
+}
+
+void MainPage::OnReplay(Object^ sender, RoutedEventArgs^ e)
+{
+    if (!strokeBuilder)
+    {
+        strokeBuilder = ref new InkStrokeBuilder();
+        inkReplayTimer = ref new DispatcherTimer();
+        inkReplayTimer->Interval = TimeSpan{ 10000000 / FPS };
+        inkReplayTimer->Tick += ref new EventHandler<Object^>(this, &MainPage::InkReplayTimer_Tick);
+    }
+
+    strokesToReplay = inkCanvas->InkPresenter->StrokeContainer->GetStrokes();
+
+    ReplayButton->IsEnabled = false;
+    inkCanvas->InkPresenter->IsInputEnabled = false;
+    ClearCanvasStrokeCache();
+
+    // Calculate the beginning of the earliest stroke and the end of the latest stroke.
+    // This establishes the time period during which the strokes were collected.
+    beginTimeOfRecordedSession = DateTime{ MAXINT64 };
+    endTimeOfRecordedSession = DateTime{ MININT64 };
+    for (InkStroke^ stroke : strokesToReplay)
+    {
+        IBox<DateTime>^ startTime = stroke->StrokeStartedTime;
+        IBox<TimeSpan>^ duration = stroke->StrokeDuration;
+        if (startTime && duration)
+        {
+            if (beginTimeOfRecordedSession.UniversalTime > startTime->Value.UniversalTime)
+            {
+                beginTimeOfRecordedSession.UniversalTime = startTime->Value.UniversalTime;
+            }
+            if (endTimeOfRecordedSession.UniversalTime < startTime->Value.UniversalTime + duration->Value.Duration)
+
+            {
+                endTimeOfRecordedSession.UniversalTime = startTime->Value.UniversalTime + duration->Value.Duration;
+            }
+        }
+    }
+
+    // If we found at least one stroke with a timestamp, then we can replay.
+    if (beginTimeOfRecordedSession.UniversalTime != MAXINT64)
+    {
+        durationOfRecordedSession = TimeSpan{ endTimeOfRecordedSession.UniversalTime - beginTimeOfRecordedSession.UniversalTime };
+
+        ReplayProgress->Maximum = static_cast<double>(durationOfRecordedSession.Duration);
+        ReplayProgress->Value = 0.0;
+        ReplayProgress->Visibility = Windows::UI::Xaml::Visibility::Visible;
+
+        beginTimeOfReplay = GetCurrentDateTime();
+        inkReplayTimer->Start();
+
+        // rootPage->NotifyUser("Replay started.", NotifyType::StatusMessage);
+    }
+    else
+    {
+        // There was nothing to replay. Either there were no strokes at all,
+        // or none of the strokes had timestamps.
+        StopReplay();
+    }
+}
+
+void MainPage::StopReplay()
+{
+    if (inkReplayTimer)
+    {
+        inkReplayTimer->Stop();
+    }
+
+    ReplayButton->IsEnabled = true;
+    inkCanvas->InkPresenter->IsInputEnabled = true;
+    ReplayProgress->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    inkToolBar->TargetInkCanvas = inkCanvas;
+}
+
+void MainPage::InkReplayTimer_Tick(Object^ sender, Object^ e)
+{
+    DateTime currentTimeOfReplay = GetCurrentDateTime();
+    TimeSpan timeElapsedInReplay = TimeSpan{ currentTimeOfReplay.UniversalTime - beginTimeOfReplay.UniversalTime };
+
+    ReplayProgress->Value = static_cast<double>(timeElapsedInReplay.Duration);
+
+    DateTime timeEquivalentInRecordedSession = DateTime{ beginTimeOfRecordedSession.UniversalTime + timeElapsedInReplay.Duration };
+    inkCanvas->InkPresenter->StrokeContainer = GetCurrentStrokesView(timeEquivalentInRecordedSession);
+    if (timeElapsedInReplay.Duration > durationOfRecordedSession.Duration)
+    {
+        StopReplay();
+        // rootPage->NotifyUser("Replay finished.", NotifyType::StatusMessage);
+    }
+}
+
+InkStrokeContainer^ MainPage::GetCurrentStrokesView(DateTime time)
+{
+    auto inkStrokeContainer = ref new InkStrokeContainer();
+
+    // The purpose of this sample is to demonstrate the timestamp usage,
+    // not the algorithm. (The time complexity of the code is O(N^2).)
+    for (auto stroke : strokesToReplay)
+    {
+        InkStroke^ s = GetPartialStroke(stroke, time);
+        if (s)
+        {
+            inkStrokeContainer->AddStroke(s);
+        }
+    }
+
+    return inkStrokeContainer;
+}
+
+InkStroke^ MainPage::GetPartialStroke(InkStroke^ stroke, DateTime time)
+{
+    IBox<DateTime>^ startTime = stroke->StrokeStartedTime;
+    IBox<TimeSpan>^ duration = stroke->StrokeDuration;
+    if (!startTime || !duration)
+    {
+        // If a stroke does not have valid timestamp, then treat it as
+        // having been drawn before the playback started.
+        // We must return a clone of the stroke, because a single stroke cannot
+        // exist in more than one container.
+        return stroke->Clone();
+    }
+
+    if (time.UniversalTime < startTime->Value.UniversalTime)
+    {
+        // Stroke has not started 
+        return nullptr;
+    }
+
+    if (time.UniversalTime >= startTime->Value.UniversalTime + duration->Value.Duration)
+    {
+        // Stroke has already ended.
+        // We must return a clone of the stroke, because a single stroke cannot exist in more than one container.
+        return stroke->Clone();
+    }
+
+    // Stroke has started but not yet ended.
+    // Create a partial stroke on the assumption that the ink points are evenly distributed in time.
+    IVectorView<InkPoint^>^ points = stroke->GetInkPoints();
+    double portion = static_cast<double>(time.UniversalTime - startTime->Value.UniversalTime) / duration->Value.Duration;
+    auto count = (int)((points->Size - 1) * portion) + 1;
+    auto initialPoints = ref new VectorView<InkPoint^>(begin(points), begin(points) + count);
+    return strokeBuilder->CreateStrokeFromInkPoints(initialPoints, float3x2::identity());
+}
+
+
--- Solution/SurfaceInk/SurfaceInk/MainPage.xaml.h
+++ Solution/SurfaceInk/SurfaceInk/MainPage.xaml.h
@@ -18,5 +18,29 @@ namespace SurfaceInk
 		MainPage();
 	private:
 		void OnSizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ e);
-	};
+        void InkPresenter_StrokesCollected(Windows::UI::Input::Inking::InkPresenter^ sender, Windows::UI::Input::Inking::InkStrokesCollectedEventArgs^ e);
+        void OnReset(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void OnReplay(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void InkReplayTimer_Tick(Platform::Object^ sender, Platform::Object^ e);
+
+        void StopReplay();
+        void ClearCanvasStrokeCache();
+        Windows::UI::Input::Inking::InkStrokeContainer^ GetCurrentStrokesView(Windows::Foundation::DateTime time);
+        Windows::UI::Input::Inking::InkStroke^ GetPartialStroke(Windows::UI::Input::Inking::InkStroke^ stroke, Windows::Foundation::DateTime time);
+
+        // MainPage^ rootPage = MainPage::Current;
+
+        static const int FPS = 60;
+
+        Windows::Foundation::DateTime beginTimeOfRecordedSession;
+        Windows::Foundation::DateTime endTimeOfRecordedSession;
+        Windows::Foundation::TimeSpan durationOfRecordedSession;
+        Windows::Foundation::DateTime beginTimeOfReplay;
+
+        Windows::UI::Xaml::DispatcherTimer^ inkReplayTimer;
+
+        Windows::UI::Input::Inking::InkStrokeBuilder^ strokeBuilder;
+        Windows::Foundation::Collections::IVectorView<Windows::UI::Input::Inking::InkStroke^>^ strokesToReplay;
+
+    };
 }
