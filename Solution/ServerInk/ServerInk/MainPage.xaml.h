66c1558b04a37882ca116076341a036067516e3d
CodingDogzxg <codingdogzxg@gmail.com>
2021-12-12 11:10:05 +0800

    TCP client part - preview

---
+++ Solution/ServerInk/ServerInk/MainPage.xaml.h
@@ -0,0 +1,54 @@
+﻿//
+// MainPage.xaml.h
+// MainPage 类的声明。
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+#include <string>
+
+namespace ServerInk
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public ref class MainPage sealed
+    {
+    public:
+        MainPage();
+        void p();
+    private:
+        void OnLoaded(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ e);
+        void StartServer();
+        void StreamSocketListener_ConnectionReceived(Windows::Networking::Sockets::StreamSocketListener^ sender, Windows::Networking::Sockets::StreamSocketListenerConnectionReceivedEventArgs^ args);
+        void OnSizeChanged(Platform::Object^ sender, Windows::UI::Xaml::SizeChangedEventArgs^ e);
+        void UpdateFrameworkSize();
+        void InkPresenter_StrokesCollected(Windows::UI::Input::Inking::InkPresenter^ sender, Windows::UI::Input::Inking::InkStrokesCollectedEventArgs^ e);
+        void OnReset(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void OnReplay();
+        void InkReplayTimer_Tick(Platform::Object^ sender, Platform::Object^ e);
+
+        void StopReplay();
+        void ClearCanvasStrokeCache();
+        Windows::UI::Input::Inking::InkStrokeContainer^ GetCurrentStrokesView(Windows::Foundation::DateTime time);
+        Windows::UI::Input::Inking::InkStroke^ GetPartialStroke(Windows::UI::Input::Inking::InkStroke^ stroke, Windows::Foundation::DateTime time);
+
+        // MainPage^ rootPage = MainPage::Current;
+
+        static const int FPS = 60;
+
+        Windows::Foundation::DateTime beginTimeOfRecordedSession;
+        Windows::Foundation::DateTime endTimeOfRecordedSession;
+        Windows::Foundation::TimeSpan durationOfRecordedSession;
+        Windows::Foundation::DateTime beginTimeOfReplay;
+
+        Windows::UI::Xaml::DispatcherTimer^ inkReplayTimer;
+
+        Windows::UI::Input::Inking::InkStrokeBuilder^ strokeBuilder;
+        Windows::Foundation::Collections::IVectorView<Windows::UI::Input::Inking::InkStroke^>^ strokesToReplay;
+        Windows::Foundation::Collections::IVectorView<Windows::UI::Input::Inking::InkStroke^>^ strokesToReplayTemp;
+
+        bool isConnected = false;
+    };
+}
---
+++ Solution/ServerInk/ServerInk/TCPServer.cpp
@@ -0,0 +1,98 @@
+#include "pch.h"
+#include "TCPServer.h"
+
+using namespace concurrency;
+
+
+TCPServer::TCPServer()
+{
+    WSADATA wsaData;
+    char name[255];
+    char* ipaddr;
+    PHOSTENT hostinfo;
+
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) == 0)
+    {
+        if (gethostname(name, sizeof(name)) == 0)
+        {
+            if ((hostinfo = gethostbyname(name)) != NULL)
+            {
+                ipaddr = inet_ntoa(*(struct in_addr*)*hostinfo->h_addr_list);
+                ip = ipaddr;
+            }
+        }
+        WSACleanup();
+    }
+}
+
+TCPServer::~TCPServer()
+{
+	CleanUp();
+}
+
+//IAsyncAction^ TCPServer::StartTcpConnection()
+//{
+//	return create_async([this, first, last]
+//		{
+//			
+//		});
+//}
+
+int TCPServer::StartTcpConnection()
+{
+	// Return value == -1 : socket error.
+
+	// Initialize a windows socket
+	/*WSAData wsData;
+	WORD ver = MAKEWORD(2, 2);
+	int wsok = WSAStartup(ver, &wsData);*/
+
+	if (wsok != 0) {
+		return -1;
+	}
+
+	// Create a socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd == INVALID_SOCKET) {
+		return -1;
+	}
+
+	// Bind the socket to an ip:port
+	/*sockaddr_in server;*/
+	server.sin_family = AF_INET;
+	server.sin_port = htons(9527);
+	server.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	bind(sockfd, (sockaddr*)&server, sizeof(server));
+
+	// Start listening
+	listen(sockfd, 1);
+	listening = true;
+	 //Wait for connection
+	/*int client_sz = sizeof(client);
+	connfd = accept(sockfd, (sockaddr*)&client, &client_sz);
+	while (true) {
+
+	}*/
+	//closesocket(sockfd);
+	return 0;
+}
+
+void TCPServer::waitForConnection() {
+	int client_sz = sizeof(client);
+	connfd = accept(sockfd, (sockaddr*)&client, &client_sz);
+	//closesocket(sockfd);
+}
+
+int TCPServer::recvFromClient() {
+	ZeroMemory(buffer, MAXRECVSIZE);
+	int n = recv(connfd, buffer, MAXRECVSIZE, 0);
+	return n;
+}
+
+void TCPServer::CleanUp()
+{
+	closesocket(connfd);
+	WSACleanup();
+}
